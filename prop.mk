b7874e4d2c39ddc18c0c28907bdedb5cb9e675ad
Ken-HV <kenhv@null.net>
2019-07-13 15:07:15 +0000

    AOSiP-ify

--- prop.mk
+++ prop.mk
@@ -10,3 +10,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
 persist.camera.dual.camera=0
 persist.camera.CDS=off
+
+#Additional
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.eis.enable=0 \
+    persist.camera.HAL3.enabled=1 \
+    debug.hwui.profile=true
