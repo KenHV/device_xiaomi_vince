97540ffb9486005a72e0c0ef3e5a078f796c3b41
KenHV <kenhv@null.net>
2019-10-06 20:19:42 +0530

    vince: Update build properties
    Signed-off-by: KenHV <kenhv@null.net>

--- aosip.mk
+++ aosip.mk
@@ -36,7 +36,8 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V10.0.4.0.OEGMIFH release-keys"
+   PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V10.2.3.0.OEGMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V10.0.4.0.OEGMIFH:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V10.2.3.0.OEGMIXM:user/release-keys"
+
