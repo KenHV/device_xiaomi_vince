7bd5dccc6549ada5f9b78e2839bac7b7ec528f93
KenHV <kenhv@null.net>
2020-03-20 15:44:54 +0000

    vince: aosip-ify
    Signed-off-by: KenHV <kenhv@null.net>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,5 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vince.mk
+    $(LOCAL_DIR)/aosip_vince.mk
 
-COMMON_LUNCH_CHOICES := \
-    lineage_vince-user \
-    lineage_vince-userdebug \
-    lineage_vince-eng
