d952d52d2d8704e4780d6957bab80f6b27ef448e
KenHV <kenhv@null.net>
2020-03-25 14:17:10 +0000

    vince: AOSiP-ify
    Signed-off-by: KenHV <kenhv@null.net>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_vince.mk
+    $(LOCAL_DIR)/aosip_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_vince-user \
-    derp_vince-userdebug \
-    derp_vince-eng
+    aosip_vince-user \
+    aosip_vince-userdebug \
+    aosip_vince-eng
